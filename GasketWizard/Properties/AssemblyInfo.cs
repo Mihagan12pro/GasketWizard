d9e4d278b56e2cce33ca4ed2de9bc9e743c155d9
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-06-04 16:23:04 +0300

    feat: Add com dependencies

--- GasketWizard/Properties/AssemblyInfo.cs
+++ GasketWizard/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1846f0b9-b42a-44a7-be74-49d8463a03d0")]
