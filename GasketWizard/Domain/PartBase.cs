34e4472759af029b72ce03017f86cbc6783bbd23
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-06-04 16:36:37 +0300

    feat: add first domain model (add first model - shim)

---
+++ GasketWizard/Domain/PartBase.cs
@@ -0,0 +1,7 @@
+﻿namespace GasketWizard.Domain
+{
+    public abstract class PartBase
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ GasketWizard/Domain/Shims/Shim.cs
@@ -0,0 +1,20 @@
+﻿namespace GasketWizard.Domain.Shims
+{
+    public class Shim : PartBase
+    {
+        /// <summary>
+        /// d
+        /// </summary>
+        public double InternalDiameter { get; set; }
+
+        /// <summary>
+        /// D
+        /// </summary>
+        public double ExternalDiameter { get; set; }
+        
+        /// <summary>
+        /// S
+        /// </summary>
+        public double Width { get; set; }
+    }
+}
