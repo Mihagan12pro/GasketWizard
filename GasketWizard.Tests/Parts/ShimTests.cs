e1862ab4adefb1ab8afd2bf6504e83d3a8fbc69b
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-06-04 17:46:05 +0300

    chore: add tests project

---
+++ GasketWizard.Tests/Parts/ShimTests.cs
@@ -0,0 +1,34 @@
+﻿using GasketWizard.Creators.Shims.Part;
+using GasketWizard.Factory;
+using Kompas6Constants;
+using KompasAPI7;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+
+namespace GasketWizard.Tests.Parts
+{
+    /// <summary>
+    /// Requires running kompas app
+    /// </summary>
+    [TestClass]
+    public class ShimTests
+    {
+        [TestMethod]
+        public void TestCreating()
+        {
+            Guid guid = Guid.NewGuid();
+
+            Type comType = Type.GetTypeFromProgID("KOMPAS.Application.7");
+
+            IApplication kompas = (IApplication)Activator.CreateInstance(comType);
+            kompas.Visible = true;
+
+            ShimsFactory shimsFactory = new ShimsFactory();
+            ShimPartCreator partCreator = shimsFactory.CreateShimPart((IPartDocument)kompas.Documents.Add(DocumentTypeEnum.ksDocumentPart));
+
+
+        }
+    }
+}
