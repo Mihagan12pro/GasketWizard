620690627636a6d3a9023502354640cf5430518d
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-06-29 11:42:25 +0300

    feat: add GasketHousing class

---
+++ GasketWizard/Domain/Housing/GasketHousing.cs
@@ -0,0 +1,10 @@
+﻿using GasketWizard.Attributes;
+
+namespace GasketWizard.Domain.Housing
+{
+    [PartGroup("Nuts", "ru-RU:Корпуса")]
+    [ModelTypeAttributes(Enums.ModelType.Part)]
+    public abstract class GasketHousing : PartBase
+    {
+    }
+}
